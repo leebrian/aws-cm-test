1903ff19fe7b47376168b3da616139950fa053b1
leebrian <brianalee@gmail.com>
2019-01-06 21:34:35 -0500

    initial cut at processing the acm responses

--- assess_comp_med.py
+++ assess_comp_med.py
@@ -17,6 +17,7 @@ import os
 import requests
 import pandas
 import json
+import numpy
 import re
 
 
@@ -30,7 +31,7 @@ def test_detect_entities(client):
 
 def dump_data_map(map):
     map_str = json.dumps(map, indent=4)
-    print(map_str)
+    # print(map_str)
     with open('doc/data-map.json', 'w', encoding='utf-8') as file:
         file.write(map_str)
 
@@ -42,7 +43,7 @@ def dump_map_to_reamde(map):
     new_readme = re.sub(r'(?is)## Data Maps.+',
                         '## Data Maps\n\n```json\n' + json.dumps(map, indent=4) + '\n```\n', readme)
 
-    print(new_readme)
+    # print(new_readme)
     with open('./README.md', 'w', encoding='utf-8') as file:
         file.write(new_readme)
 
@@ -53,7 +54,7 @@ def doc_data_map():
             "data-source": "ML-Workshop-Synthetic-Chief-Complaints",
             "data-owner": "https://gitlab.com/gte577z",
             "data-custodian": "https://gitlab.com/gte577z",
-            "address": "https://gitlab.com/gte577z/ml-workshop/raw/master/dat/text.csv",
+            "address": "https://gitlab.com/gte577z/ml-workshop/raw/master/data/text.csv",
             "extract-date": "2018-12-20",
             "format": "CSV",
             "methods": "https://doi.org/10.1038/s41746-018-0070-0",
@@ -99,7 +100,7 @@ def load_cc_df():
 
 
 def call_aws(client):
-    """ probably cost money"""
+    """ most like to cost money since free tier stops around 25k"""
     # aws free tier is limited so I just want to call for every text, save off mapped, and process the results later
     text = load_cc_df()
     print('describe synthetic chief complaint mapping dataframe : \n' +
@@ -114,24 +115,153 @@ def call_aws(client):
             text.loc[i, 'result_file'] = filename
 
     # cleanup and write out what has been processed
-    text.to_csv('dat/text-mapped.csv', index=False)
+    text.to_csv('dat/text-mapped.csv', index=False, encoding='utf-8')
 
 
 def process_acm():
+    """
+    Limited in that it will only capture the last trait for dx, symptom, or sign. Might want to switch to json because of csv limitation.
+    """
     map_df = pandas.read_csv('dat/text-mapped.csv')
     acm_df = pandas.DataFrame(data=map_df.loc[:, ['text', 'diagnosis']], columns=[
         "text",
         "diagnosis",
-        "aws-text",
-        "aws-score",
-        "aws-category",
-        "aws-category",
-        "aws-trait"
+        "aws-entity-count",
+        "aws-dx-count",
+        "aws-dx-text",
+        "aws-dx-score",
+        "aws-dx-category",
+        "aws-dx-type",
+        "aws-dx-trait-name",
+        "aws-dx-trait-score",
+        "aws-sy-count",
+        "aws-sy-text",
+        "aws-sy-score",
+        "aws-sy-category",
+        "aws-sy-type",
+        "aws-sy-trait-name",
+        "aws-sy-trait-score",
+        "aws-sign-count",
+        "aws-sign-text",
+        "aws-sign-score",
+        "aws-sign-category",
+        "aws-sign-type",
+        "aws-sign-trait-name",
+        "aws-sign-trait-score"
     ])
     assert len(map_df) == len(
         acm_df), 'text mapped and acm text should be same length'
-    print('len map_df ' + str(len(map_df)))
-    print('len acm_df ' + str(len(acm_df)))
+    # print('len map_df ' + str(len(map_df)))
+    # print('len acm_df ' + str(len(acm_df)))
+
+    for row in map_df.itertuples():
+        with open('results/'+row.result_file, encoding='utf-8') as file:
+            result = json.load(file)
+            acm_df.loc[row.Index, 'aws-entity-count'] = len(result['Entities'])
+            for entity in result['Entities']:
+                dx_count = 0
+                sym_count = 0
+                sign_count = 0
+                for trait in entity['Traits']:
+                    if trait['Name'] == 'DIAGNOSIS':
+                        dx_count += 1
+                        acm_df.loc[row.Index, 'aws-dx-text'] = entity['Text']
+                        acm_df.loc[row.Index, 'aws-dx-score'] = entity['Score']
+                        acm_df.loc[row.Index,
+                                   'aws-dx-category'] = entity['Category']
+                        acm_df.loc[row.Index, 'aws-dx-type'] = entity['Type']
+                        acm_df.loc[row.Index,
+                                   'aws-dx-trait-name'] = trait['Name']
+                        acm_df.loc[row.Index,
+                                   'aws-dx-trait-score'] = trait['Score']
+                    elif trait['Name'] == 'SYMPTOM':
+                        sym_count += 1
+                        acm_df.loc[row.Index, 'aws-sy-text'] = entity['Text']
+                        acm_df.loc[row.Index, 'aws-sy-score'] = entity['Score']
+                        acm_df.loc[row.Index,
+                                   'aws-sy-category'] = entity['Category']
+                        acm_df.loc[row.Index, 'aws-sy-type'] = entity['Type']
+                        acm_df.loc[row.Index,
+                                   'aws-sy-trait-name'] = trait['Name']
+                        acm_df.loc[row.Index,
+                                   'aws-sy-trait-score'] = trait['Score']
+                    elif trait['Name'] == 'SIGN':
+                        sign_count += 1
+                        acm_df.loc[row.Index, 'aws-sign-text'] = entity['Text']
+                        acm_df.loc[row.Index,
+                                   'aws-sign-score'] = entity['Score']
+                        acm_df.loc[row.Index,
+                                   'aws-sign-category'] = entity['Category']
+                        acm_df.loc[row.Index, 'aws-sign-type'] = entity['Type']
+                        acm_df.loc[row.Index,
+                                   'aws-sign-trait-name'] = trait['Name']
+                        acm_df.loc[row.Index,
+                                   'aws-sign-trait-score'] = trait['Score']
+
+                acm_df.loc[row.Index, 'aws-dx-count'] = dx_count
+                acm_df.loc[row.Index, 'aws-sy-count'] = sym_count
+                acm_df.loc[row.Index, 'aws-sign-count'] = sign_count
+
+    acm_df['aws-entity-count'] = pandas.to_numeric(
+        acm_df['aws-entity-count'], downcast='integer')
+    acm_df['aws-dx-count'] = acm_df['aws-dx-count'].fillna(0).astype(int)
+    acm_df['aws-sy-count'] = acm_df['aws-sy-count'].fillna(0).astype(int)
+    acm_df['aws-sign-count'] = acm_df['aws-sign-count'].fillna(0).astype(int)
+
+    dump_acm_text(acm_df)
+
+    return map_df, acm_df
+
+
+def what_unique_attribute(df, attribute):
+    types = []
+    for row in df.itertuples():
+        with open('results/'+row.result_file, encoding='utf-8') as file:
+            result = json.load(file)
+            for entity in result['Entities']:
+                if entity[attribute] not in types:
+                    types.append(entity[attribute])
+
+    return types
+
+
+def what_unique_traits(df):
+    traits = []
+    for row in df.itertuples():
+        with open('results/'+row.result_file, encoding='utf-8') as file:
+            result = json.load(file)
+            for entity in result['Entities']:
+                for trait in entity['Traits']:
+                    tuple = (entity['Type'], entity['Category'], trait['Name'])
+                    if tuple not in traits:
+                        traits.append(tuple)
+
+    return traits
+
+
+def dump_acm_text(df):
+    df.to_csv('dat/text-acm.csv', index=False,
+              encoding='utf-8')
+
+
+def doc_acm_text(map, df):
+    with open('doc/notes.txt', 'w', encoding='utf-8') as file:
+
+        file.write('Unique types ' + str(what_unique_attribute(map, 'Type')))
+        file.write('\nUnique categories ' +
+                   str(what_unique_attribute(map, 'Category')))
+        file.write('\nUnique trait names ' + str(what_unique_traits(map)))
+        file.write('\nacm text dataframe description\n' +
+                   df.describe().to_string())
+
+        file.write('\nEntity count frequency distribution\n' +
+                   df['aws-entity-count'].value_counts().to_string())
+        file.write('\nDiagnosis count frequency distribution\n' +
+                   df['aws-dx-count'].value_counts().to_string())
+        file.write('\nSymptom count frequency distribution\n' +
+                   df['aws-sy-count'].value_counts().to_string())
+        file.write('\nSign count frequency distribution\n' +
+                   df['aws-sign-count'].value_counts().to_string())
 
 
 def main():
@@ -146,7 +276,9 @@ def main():
     call_aws(client)
 
     # todo process mapping
-    process_acm()
+    map, text = process_acm()
+
+    doc_acm_text(map, text)
 
     # todo make sense of it
 
--- readme.md
+++ readme.md
@@ -6,6 +6,16 @@ Using Scott Lee's public data sets from <https://gitlab.com/gte577z/ml-workshop>
 
 Question- how does CM diagnosis compare to process that generated these codes using the process described in [Scott's paper](https://www.nature.com/articles/s41746-018-0070-0)? Using the set of 42,364 synthetic chief complaints and HCUP CCS diagnosis codes.
 
+## Results
+
+Multiple different types of entity types found ['SYSTEM_ORGAN_SITE', 'DX_NAME', 'ACUITY', 'DIRECTION', 'ID', 'PROCEDURE_NAME', 'TEST_NAME', 'TREATMENT_NAME', 'PHONE_OR_FAX', 'GENERIC_NAME', 'ADDRESS', 'BRAND_NAME', 'DATE', 'NAME', 'PROFESSION']
+
+Multiple different types of entity categories found ['ANATOMY', 'MEDICAL_CONDITION', 'PROTECTED_HEALTH_INFORMATION', 'TEST_TREATMENT_PROCEDURE', 'MEDICATION']
+
+Multiple different types of trait names found ['SYMPTOM', 'DIAGNOSIS', 'SIGN', 'NEGATION']
+
+Multiple different tuples of type, caegory, trait found [('DX_NAME', 'MEDICAL_CONDITION', 'SYMPTOM'), ('DX_NAME', 'MEDICAL_CONDITION', 'DIAGNOSIS'), ('DX_NAME', 'MEDICAL_CONDITION', 'SIGN'), ('DX_NAME', 'MEDICAL_CONDITION', 'NEGATION')]
+
 ## Requirements
 
 Remember to [configure your local environment](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html#cli-quick-configuration) with an aws access key, secret key, and default region. Entire exploration done using free tier (thanks AWS) and in the us-east-1 region, although I don't think that matters.
@@ -31,7 +41,7 @@ Remember to [configure your local environment](https://docs.aws.amazon.com/cli/l
         "data-source": "ML-Workshop-Synthetic-Chief-Complaints",
         "data-owner": "https://gitlab.com/gte577z",
         "data-custodian": "https://gitlab.com/gte577z",
-        "address": "https://gitlab.com/gte577z/ml-workshop/raw/master/dat/text.csv",
+        "address": "https://gitlab.com/gte577z/ml-workshop/raw/master/data/text.csv",
         "extract-date": "2018-12-20",
         "format": "CSV",
         "methods": "https://doi.org/10.1038/s41746-018-0070-0",
